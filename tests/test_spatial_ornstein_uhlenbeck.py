81ff0fe34c6054c6373e0d97a89cd0d46d1fae86
Jesse Zaneveld <zaneveld@gmail.com>
2016-09-30 15:20:33 -0700

    Added tests for the Perturbation object

--- tests/test_spatial_ornstein_uhlenbeck.py
+++ tests/test_spatial_ornstein_uhlenbeck.py
@@ -55,7 +55,7 @@ class TestProcess(unittest.TestCase):
         npt.assert_almost_equal(start_coord,end_coord)
 
     def test_perturb_stable_process(self):
-        """Perturb a stable process to seek a new attractor"""
+        """Process can be perturbed to become more variable"""
         stable_process = self.TestProcesses["stable_process"]
         start_coord = stable_process.Coord
         
@@ -81,14 +81,118 @@ class TestProcess(unittest.TestCase):
         #which becomes pure Brownian motion 
         #diverges more over time than the stable process  
         stable_change = abs(unperturbed_coord - start_coord)
-        print stable_change
 
         unstable_change = abs(end_coord - unperturbed_coord)
-        print unstable_change
 
         self.assertTrue(stable_change < unstable_change)
  
+    def test_perturb_alter_mean(self):
+        """Process can be perturbed to take on a new mean location"""
+        stable_process = self.TestProcesses["stable_process"]
+        start_coord = stable_process.Coord
         
+        a_long_time = 1000
+        
+        for i in range(a_long_time):
+            stable_process.update(1.0)
+        unperturbed_coord = stable_process.Coord
+
+        new_mu = -0.5 
+        perturbation = Perturbation(start=0,end=0,params={"mu":new_mu},update_mode="replace")
+        stable_process.Perturbations.append(perturbation)
+        for i in range(a_long_time):
+            stable_process.update(1.0)
+        end_coord = stable_process.Coord
+
+        #We will test that Perturbations actually update
+        #process parameters during simulation elsewhere
+        
+        #Here I just want to confirm that after running for a while 
+        #altering mu moves the process closer to its new mean 
+        stable_diff = abs(unperturbed_coord - new_mu)
+        unstable_diff = abs(end_coord - new_mu)
+
+        self.assertTrue(unstable_diff < stable_diff)
+ 
+
+
+
+class TestPerturbation(unittest.TestCase):
+    """Tests of the Process class"""
+
+    def setUp(self):
+        
+        #Set up some common perturbations to test
+        
+        perturbation_start = 50
+        perturbation_end = 75
+
+        
+        set_low_lambda = Perturbation(perturbation_start,perturbation_end,params={"lambda":0.005},update_mode = "replace")
+        set_zero_lambda = Perturbation(perturbation_start,perturbation_end,params={"lambda":0.0},update_mode = "replace")
+        double_lambda = Perturbation(perturbation_start,perturbation_end,params={"lambda":2.0},update_mode = "multiply")
+        halve_lambda = Perturbation(perturbation_start,perturbation_end,params={"lambda":0.5},update_mode = "multiply")
+        add_to_lambda = Perturbation(perturbation_start,perturbation_end,params={"lambda":0.1},update_mode = "add")
+        subtract_from_lambda = Perturbation(perturbation_start,perturbation_end,params={"lambda":0.1},update_mode = "add")
+
+        self.TestPerturbations ={"set_low_lambda":set_low_lambda,"set_zero_lambda":set_zero_lambda,"double_lambda":double_lambda,\
+          "halve_lambda":halve_lambda,"add_to_lambda":add_to_lambda,"subtract_from_lambda":subtract_from_lambda}
+
+        
+
+    def test_isActive(self):
+        """Perturbation isActive tests whether it is active based on the current time"""
+        #perturbations start at 50 and end at 75
+        exact_start = 50
+        exact_end = 75
+        in_the_middle = 55
+        start_of_experiment = 0
+
+        curr_perturbation = self.TestPerturbations["set_zero_lambda"]
+        self.assertTrue(curr_perturbation.isActive(in_the_middle))
+        self.assertFalse(curr_perturbation.isActive(start_of_experiment))
+        
+        #let's test the borderline cases, which should be active
+        self.assertTrue(curr_perturbation.isActive(exact_start))
+        self.assertTrue(curr_perturbation.isActive(exact_end))
+
+        #Test that if we change Start and End we change
+        #isActive results
+
+        curr_perturbation.Start = 90
+        curr_perturbation.End = 100
+        
+        self.assertFalse(curr_perturbation.isActive(55))
+        self.assertTrue(curr_perturbation.isActive(95)) 
+
+    def test_updateParam_updates_single_param(self):
+        """Perturbation.updateParams updates a single parameter correctly"""
+
+        base_params = {"mu":0.1,"lambda":0.25,"delta":0.18}
+        
+        curr_perturbation = self.TestPerturbations["set_low_lambda"]
+        obs = curr_perturbation.updateParams(base_params)
+        exp = {"mu":0.1,"lambda":0.005,"delta":0.18}
+        self.assertEqual(obs,exp)
+
+        curr_perturbation = self.TestPerturbations["set_zero_lambda"]
+        obs = curr_perturbation.updateParams(base_params)
+        exp = {"mu":0.1,"lambda":0.0,"delta":0.18}
+        self.assertEqual(obs,exp)
+
+        curr_perturbation = self.TestPerturbations["double_lambda"]
+        obs = curr_perturbation.updateParams(base_params)
+        exp = {"mu":0.1,"lambda":0.50,"delta":0.18}
+        self.assertEqual(obs,exp)
+        
+        curr_perturbation = self.TestPerturbations["halve_lambda"]
+        obs = curr_perturbation.updateParams(base_params)
+        exp = {"mu":0.1,"lambda":0.125,"delta":0.18}
+        self.assertEqual(obs,exp)
+
+
+
+
 if __name__ == '__main__':
     unittest.main()
 
