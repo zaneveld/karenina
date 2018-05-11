b814b9e01c3c5c389cd657bc1c6f1b062ab637ab
SLPeoples <33819909+SLPeoples@users.noreply.github.com>
2018-05-11 11:39:04 -0700

    update tests to be more fluid

--- tests/test_experiment.py
+++ tests/test_experiment.py
@@ -38,64 +38,25 @@ class TestExperiment(unittest.TestCase):
         self.treatment_params = [[],[]]
         self.interindividual_variation = 0.01
 
-        # Prepare variables from init
-
-        #for i,n in enumerate(n_individuals):
-        for i,n in enumerate(self.NIndividuals):
-            self.Treatments[i]["n_individuals"] = n
-
-        self.NTimepoints = self.n_timepoints
-
-        colors = ['fuchsia','cyan','darkorange','blue','yellow']
-        #Set up the experimental subjects
-        for treatment_idx,treatment in enumerate(self.Treatments):
-
-            #Set a color for each treatment
-            individuals = []
-            params = copy(self.BaseParams)
-
-            if treatment_idx < len(colors):
-                params['color'] = colors[treatment_idx]
-            else:
-                params['color'] = 'lightgray'
-
-            print(treatment)
-            for i in range(treatment["n_individuals"]):
-
-                curr_subject_id = "%s_%i" %(treatment["treatment_name"],i)
-                curr_subject = Individual(subject_id = curr_subject_id,
-                  params = params,\
-                  metadata={"treatment":treatment["treatment_name"]},\
-                  interindividual_variation=self.interindividual_variation)
-                individuals.append(curr_subject)
-            treatment["individuals"] = individuals
-
-
-        #Set up the treatment parameters
-        for treatment_idx,treatment in enumerate(self.Treatments):
-            treatment["perturbations"] = []
-            treatment["active_perturbations"] = []
-            raw_perturbation_info = self.treatment_params[treatment_idx]
-            #We should have a dict with start, end, and parms for each perturbation
-            print ("raw_perturbation_info:",raw_perturbation_info)
-            for p in raw_perturbation_info:
-                print ("params:",p)
-                curr_perturbation = Perturbation(p["start"],p["end"],p["params"],p["update_mode"],p["axes"])
-                treatment["perturbations"].append(curr_perturbation)
-
-        #Set up a place to hold data on the experiment outcome
-
-        coords = ["x","y","z"]
-        headers = "\t".join(["SampleID"]+coords)+"\n"
-        self.Data = [headers]
-
     def test_check_variable_specified_per_treatment(self):
-        Experiment.check_variable_specified_per_treatment(self,self.NIndividuals)
+        Experiment.check_variable_specified_per_treatment(Experiment(self.TreatmentNames,
+                                                                     self.NIndividuals,
+                                                                     self.n_timepoints,
+                                                                     self.BaseParams,
+                                                                     self.treatment_params,
+                                                                     self.interindividual_variation),
+                                                          self.NIndividuals)
         self.TreatmentNames.append('Error')
         self.assertRaises(ValueError, Experiment.check_variable_specified_per_treatment,self,self.NIndividuals)
 
     def test_check_n_timepoints_is_int(self):
-        Experiment.check_n_timepoints_is_int(self,self.n_timepoints)
+        Experiment.check_n_timepoints_is_int(Experiment(self.TreatmentNames,
+                                                        self.NIndividuals,
+                                                        self.n_timepoints,
+                                                        self.BaseParams,
+                                                        self.treatment_params,
+                                                        self.interindividual_variation),
+                                             self.n_timepoints)
         self.n_timepoints = [10]
         self.assertRaises(ValueError, Experiment.check_n_timepoints_is_int, self, self.n_timepoints)
 
@@ -110,9 +71,15 @@ class TestExperiment(unittest.TestCase):
         # Travis-CI Uses Xwindows backend, this prevents that issue.
         import matplotlib
         matplotlib.use('Agg')
-        
+
         self.output_folder = "./"
-        Experiment.write_to_movie_file(self, self.output_folder)
+        Experiment.write_to_movie_file(Experiment(self.TreatmentNames,
+                                                  self.NIndividuals,
+                                                  self.n_timepoints,
+                                                  self.BaseParams,
+                                                  self.treatment_params,
+                                                  self.interindividual_variation),
+                                       self.output_folder)
         assert os.path.exists("./simulation_video.mp4")
         os.remove("./simulation_video.mp4")
 
